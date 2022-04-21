f37c2de3c67ee910646a029a006a852fc0b7d553
Julieta Delgado <julidmeza@gmail.com>
2022-04-21 00:40:21 -0500

    Services | Requirement 2 | Add test to fail

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -6,11 +6,22 @@ describe('Test for UserService', () => {
     // 1. Esta clase deberá tener un método `create` que se pueda usar sin instanciar (static method), y que reciba solo los parámetros del `id`, `username` y `name`. 
     // 2. El valor de `bio` deberá ser por default para todos los `user` creados.
 
-    test('Create a new user using the UserService', () => {
+    test('1. Create a new user using the UserService', () => {
         const user = UserService.create(1, 'julietadelgado', 'Julieta Delgado')
         expect(user.id).toBe(1)
         expect(user.username).toBe('julietadelgado')
         expect(user.name).toBe('Julieta Delgado')
         expect(user.bio).not.toBeUndefined()
+    })
+
+    //Requerimiento 2: Agregar un nuevo método estático en `UserService` llamado `getInfo` que al recibir un objeto de la clase `User`, me regrese una lista con todos los valores de los atributos de dicho objeto.
+    test('2. Get all user data in a list', () => {
+        const user = UserService.create(1, 'julietadelgado', 'Julieta Delgado')
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe('julietadelgado')
+        expect(userInfoList[2]).toBe('Julieta Delgado')
+        expect(userInfoList[3]).toBe('Without bio')
+
     })
 })
