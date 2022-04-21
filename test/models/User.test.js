2423100738331bab9f593662e7314eaddc385094
Julieta Delgado <julidmeza@gmail.com>
2022-04-20 22:01:48 -0500

    Requirement 2, Step one of TDD: Write a test for the next bit of functionality you want to add

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,12 +2,12 @@ const User = require('./../../app/models/User')
 
 describe('Unit test for User Class', () => {
     test('1. Create an User object', () => {
-        const user = new User(1, 'julietadelgado', 'Julieta Delgado', 'Bio', "dateCreated", 'lastUpdated')
+        const user = new User(1, 'julietadelgado', 'Julieta Delgado', 'Bio')
         expect(user.id).toBe(1)
         expect(user.username).toBe('julietadelgado')
         expect(user.name).toBe('Julieta Delgado')
         expect(user.bio).toBe('Bio')
-        expect(user.dateCreated).toBe('dateCreated')
-        expect(user.lastUpdated).toBe('lastUpdated')
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
